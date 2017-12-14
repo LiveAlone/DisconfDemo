5157b39e30828c8f740476fadb58a4c98e315bd4
yaoqijun <yaoqijun@changingedu.com>
2017-12-14 14:10:34 +0800

    add main condition

---
+++ src/main/java/org/yqj/disconf/demo/source/ClientMain.java
@@ -0,0 +1,12 @@
+package org.yqj.disconf.demo.source;
+
+import com.baidu.disconf.client.config.ConfigMgr;
+
+/**
+ * @author yaoqijun on 2017-12-14.
+ */
+public class ClientMain {
+    public static void main(String[] args) {
+        ConfigMgr.main(null);
+    }
+}
